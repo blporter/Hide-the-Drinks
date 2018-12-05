00102e67d41053b6df52edab7432d0ee30622281
Adam <adamalanahrens@gmail.com>
2018-12-04 19:46:31 -0800

    one

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-	private float _moveHorizontal;
-	private float _moveVertical;
-	private Vector3 _movement;
-	private const float MovementSpeed = 3f;
-	private const float TurningSpeed = 20f;
-	private Rigidbody _playerRigidbody;
+public class PlayerMovement : MonoBehaviour
+{
+    private float _moveHorizontal;
+    private float _moveVertical;
+    private Vector3 _movement;
+    private const float MovementSpeed = 3f;
+    private const float TurningSpeed = 20f;
+    private Rigidbody _playerRigidbody;
+    private Boolean _isJumping = false;
 
-	// Use this for initialization
-	void Start() {
-		_playerRigidbody = GetComponent<Rigidbody>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
 
-	// Update is called once per frame
-	void Update() {
-		_moveHorizontal = Input.GetAxisRaw("Horizontal");
-		_moveVertical = Input.GetAxisRaw("Vertical");
+    // Update is called once per frame
+    void Update()
+    {
+        _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _moveVertical = Input.GetAxisRaw("Vertical");
 
-		_movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
-	}
+        _movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
 
-	private void FixedUpdate() {
-		if (_movement != Vector3.zero) {
-			Quaternion targetRotation = Quaternion.LookRotation(_movement, Vector3.up);
-			Quaternion newRotation =
-				Quaternion.Lerp(_playerRigidbody.rotation, targetRotation, TurningSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJumping)
+        {
+            _playerRigidbody.AddForce(0f, 200f, 0f);
+            _isJumping = true;
+        }
 
-			_playerRigidbody.MoveRotation(newRotation);
-			_playerRigidbody.MovePosition(transform.position + transform.forward * Time.deltaTime * MovementSpeed);
-		}
+        // Dash
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _playerRigidbody.AddForce(_movement.x * 200f, 0f, _movement.z * 200f);
+        }
+    }
 
-		// Jump
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			_playerRigidbody.AddForce(0f, 200f, 0f);
-		}
+    private void FixedUpdate()
+    {
+        if (_movement != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(_movement, Vector3.up);
+            Quaternion newRotation =
+                Quaternion.Lerp(_playerRigidbody.rotation, targetRotation, TurningSpeed * Time.deltaTime);
 
-		// Dash
-		if (Input.GetKeyDown(KeyCode.LeftShift)) {
-			_playerRigidbody.AddForce(_movement.x * 200f, 0f, _movement.z * 200f);
-		}
-	}
+            _playerRigidbody.MoveRotation(newRotation);
+            _playerRigidbody.MovePosition(transform.position + transform.forward * Time.deltaTime * MovementSpeed);
+        }
+
+        if (_isJumping)
+        {
+            GameObject ground = GameObject.FindGameObjectWithTag("PlayerCar").gameObject;
+            ground.
+        }
+    }
 }
