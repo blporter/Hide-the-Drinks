0515172acb2c7f88154a8a04cabb97ef6e7077bb
blporter <brookeporter13@gmail.com>
2019-01-10 14:54:29 -0800

    Roof fade and test model for Stevie

---
+++ Assets/Scripts/FadeTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FadeTrigger : MonoBehaviour {
+
+	[SerializeField] private GameObject _object;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			_object.SetActive(false);
+		}
+	}
+	
+	private void OnTriggerExit(Collider other) {
+		_object.SetActive(true);
+	}
+}
